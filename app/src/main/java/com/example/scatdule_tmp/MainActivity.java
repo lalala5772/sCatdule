4c446cc2964f2429c59759c22c7e95ca7153382d
mir <mir@mirui-MacBookPro.local>
2022-05-24 22:52:28 +0900

    REFACTOR

--- app/src/main/java/com/example/scatdule_tmp/MainActivity.java
+++ app/src/main/java/com/example/scatdule_tmp/MainActivity.java
@@ -1,19 +1,23 @@
 package com.example.scatdule_tmp;
 
+import static java.sql.DriverManager.println;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -29,13 +33,16 @@ public class MainActivity extends AppCompatActivity {
     Context context;
 
 
+    public boolean exp_flag = false;
+    public static int exp = 50;
     public static NoteDatabase noteDatabase = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //이동
+//      세팅 버튼 기능 구현
         ImageButton setting_btn = (ImageButton) findViewById(R.id.settings);
         setting_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,42 +52,48 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //todolist 기능 구현을 위한 코드 -> MainFragment 코드와 related됨
-        mainFragment = new MainFragment();
 
+        setDays_info(); //오늘의 날짜 가져오는 함수 선언
+        set_progress(exp); //경험치 함수 선언
+
+
+
+//        CheckBox checkBox = (CheckBox) findViewById(R.id.checkBox) ;
+//        checkBox.setOnClickListener(new CheckBox.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (((CheckBox)v).isChecked()) {
+//                    if(exp_flag)
+//                    exp+=20;
+//                    exp%=100;
+//                    set_progress(exp);
+//                    exp_flag=true;
+//
+//                } else {
+//                    if(exp_flag){
+//                        exp-=20;
+//                        exp%=100;
+//                        set_progress(exp);
+//                        exp_flag = false;
+//                    }
+//                }
+//            }
+//        }) ;
+
+        //    ---------------------------------------------------------------------------------------------
+        //    todolist 기능 구현 코드
+        mainFragment = new MainFragment();
         getSupportFragmentManager().beginTransaction().replace(R.id.container,mainFragment).commit();
 
         Button saveButton = findViewById(R.id.saveButton);
         saveButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
-
                 saveToDo();
-
                 Toast.makeText(getApplicationContext(),"추가되었습니다.",Toast.LENGTH_SHORT).show();
             }
         });
         openDatabase();
-
-
-
-
-
-
-
-        //day 정보 가져오는 코드
-        //나중에 따로 함수나 클래스로 만들어서 다른 파일로 빼낼 예정
-        days_info = findViewById(R.id.days_info);
-
-        Date currentTime = Calendar.getInstance().getTime();
-        String date_text = new SimpleDateFormat("yyyy-MM-dd    EE", Locale.getDefault()).format(currentTime);
-
-        days_info.setText(date_text);
-
-        //상태바 표시 -> 제대로 구현할 때, 100이 넘을 경우 예외처리 필요
-        int tmp_progress=30;
-        ProgressBar progress = (ProgressBar) findViewById(R.id.progress) ;
-        progress.setProgress(tmp_progress) ;
 
     }
     private void saveToDo(){
@@ -125,6 +138,27 @@ public class MainActivity extends AppCompatActivity {
             noteDatabase.close();
             noteDatabase = null;
         }
+    }
+
+    //      ----------------------------------------------------------------------------------------------
+    //      오늘의 날짜 정보 가져오는 코드
+
+    public void setDays_info(){
+        days_info = findViewById(R.id.days_info);
+
+        Date currentTime = Calendar.getInstance().getTime();
+        String date_text = new SimpleDateFormat("yyyy-MM-dd    EE", Locale.getDefault()).format(currentTime);
+
+        days_info.setText(date_text);
+
+    }
+
+    //      ----------------------------------------------------------------------------------------------
+    //      경험치 바 구현 코드
+    public void set_progress(int exp){
+        ProgressBar progress = (ProgressBar) findViewById(R.id.progress);
+        progress.setMax(100);
+        progress.setProgress(exp);
     }
 
 }
