77941d3f55b62c53d0f03edf21ff090a3791486e
mir <mir@mirui-MacBookPro.local>
2022-05-29 16:52:41 +0900

    settings->home

--- app/src/main/java/com/example/scatdule_tmp/SettingActivity.java
+++ app/src/main/java/com/example/scatdule_tmp/SettingActivity.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingActivity extends AppCompatActivity {
     private ImageButton back_btn;
+    private ImageButton home_btn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
@@ -16,7 +17,14 @@ public class SettingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         ImageButton setting_btn = (ImageButton) findViewById(R.id.backButton);
+        ImageButton setting_btn2 = (ImageButton) findViewById(R.id.home);
         setting_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        setting_btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
