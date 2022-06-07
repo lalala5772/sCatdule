d261cf7534b6b54dde9d951ac79c8c8f12f65432
mir <mir@mirui-MacBookPro.local>
2022-06-07 21:02:26 +0900

    splash

---
+++ app/src/main/java/com/example/scatdule_tmp/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.scatdule_tmp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView gif_image = (ImageView) findViewById(R.id.gif_image);
+        Glide.with(this).load(R.drawable.startview).into(gif_image);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent main = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(main);
+                finish();
+            }
+        }, 3000);
+
+    }
+
+}
